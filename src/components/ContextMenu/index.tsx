4bff9ee064208fc1cdd667018ebf8afd7cbe79c7
beigeyoon <beige.yoon@gmail.com>
2024-04-16 00:10:44 +0900

    feat. ContextMenu 컴포넌트 생성

--- src/components/ContextMenu/index.tsx
+++ src/components/ContextMenu/index.tsx
@@ -1,5 +1,69 @@
-const ContextMenu = (outerRef: React.MutableRefObject<HTMLDivElement>) => {
+import { PropsWithChildren, useState, useEffect, KeyboardEvent, useRef, useCallback } from "react";
 
+type ContextMenuItem = {
+  id: string;
+  caption: string;
+  onClick: () => void;
+}
+
+interface IContextMenu {
+  id: string;
+  items: ContextMenuItem[];
+}
+
+const ContextMenu = ({ id, items, children }: PropsWithChildren<IContextMenu>) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [position, setPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const handleContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsVisible(true);
+    setPosition({ x: e.clientX, y: e.clientY });
+  }
+
+  const keyDownHandler = useCallback((e: WindowEventMap['keydown']) => {
+    if (e.code === 'Escape') setIsVisible(false);
+  }, [])
+
+  const clickHandler = useCallback((e: MouseEvent) => {
+    if (isVisible) {
+      const rect = ulRef.current?.getBoundingClientRect();
+      if (rect) {
+        if (e.clientX < rect.left || e.clientX > rect.right || e.clientY > rect.top || e.clientY < rect.bottom) {
+          setIsVisible(false);
+        }
+      }
+    }
+  }, [isVisible])
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler);
+    window.addEventListener('click', clickHandler);
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler);
+      window.removeEventListener('click', clickHandler);
+    }
+  }, [keyDownHandler, clickHandler]);
+
+  return (
+    <>
+      <div onContextMenu={handleContextMenu}>{children}</div>
+      {isVisible && (
+        <ul style={{ left: position.x, top: position.y }} className='absolute' ref={ulRef}>
+          {items.map((item: ContextMenuItem) => (
+            <li key={item.id} onClick={() => {
+              setIsVisible(false);
+              item.onClick();
+            }}>
+              {item.caption}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
 }
 
 export default ContextMenu;
--- src/containers/Sidebar/Lists/ListButton/index.tsx
+++ src/containers/Sidebar/Lists/ListButton/index.tsx
@@ -1,6 +1,7 @@
 import CircleIcon from "@/src/components/CircleIcon";
 import { List } from "@/src/types";
 import { deleteList } from "@/src/services/list";
+import ContextMenu from "@/src/components/ContextMenu";
 
 interface IListButton {
   list: List;
@@ -10,18 +11,27 @@ const ListButton = ({ list }: IListButton) => {
   const { id, color, icon, name, items } = list;
 
   return (
-    <button
-      className='w-[123px] flex flex-col justify-between bg-gray200 rounded-xl p-[10px]'
-      onContextMenu={(e) => {
-        e.preventDefault();
-      }}
+    <ContextMenu
+      id={`list-button-${id}`}
+      items={[
+        {
+          id: 'entry-1',
+          caption: 'this is the first entry',
+          onClick: () => alert('hello!'),
+        }
+      ]}
     >
-      <div className="w-full flex items-center justify-between">
-        <CircleIcon iconName={icon} colorName={color} size='large' />
-        <span className='font-bold text-lg'>{items?.length}</span>
-      </div>
-      <span className='font-bold mt-[8px]'>{name}</span>
-    </button>
+      <button
+        className='w-[123px] flex flex-col justify-between bg-gray200 rounded-xl p-[10px]'
+      >
+        <div className="w-full flex items-center justify-between">
+          <CircleIcon iconName={icon} colorName={color} size='large' />
+          <span className='font-bold text-lg'>{items?.length}</span>
+        </div>
+        <span className='font-bold mt-[8px]'>{name}</span>
+      </button>
+    </ContextMenu>
+    
   )
 }
 
